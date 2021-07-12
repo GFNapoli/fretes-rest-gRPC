ebdb2419e27d9e27e99d395d6a920a7d38b74317
Gustavo Napoli <gnkenshin@gmail.com>
2021-07-12 15:36:11 -0300

    tratamento de erros

--- src/main/kotlin/br/com/zup/edu/CalculadoresDeFretesController.kt
+++ src/main/kotlin/br/com/zup/edu/CalculadoresDeFretesController.kt
@@ -1,8 +1,14 @@
 package br.com.zup.edu
 
+
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.exceptions.HttpStatusException
 import javax.inject.Inject
 
 @Controller
@@ -15,9 +21,29 @@ class CalculadoresDeFretesController(@Inject val grpcClient: FretesServiceGrpc.F
             .setCep(cep)
             .build()
 
-        val response = grpcClient.calculaFrete(request)
+        try {
+            val response = grpcClient.calculaFrete(request)
 
-        return FreteResponse(response.cep, response.valor)
+            return FreteResponse(response.cep, response.valor)
+        }catch (e: StatusRuntimeException){
+            val status = e.status
+            val statusCode = status.code
+            val description = status.description
+
+            if (statusCode == Status.Code.INVALID_ARGUMENT){
+                throw HttpStatusException(HttpStatus.BAD_REQUEST, description)
+            }
+            if (statusCode == Status.Code.PERMISSION_DENIED){
+                val statusProto =
+                    StatusProto.fromThrowable(e) ?: throw HttpStatusException(HttpStatus.FORBIDDEN, description)
+
+                val anyDetails = statusProto.detailsList?.get(0)
+                val erroDetails = anyDetails!!.unpack(ErroDatails::class.java)
+
+                throw HttpStatusException(HttpStatus.FORBIDDEN, "${erroDetails.code}: ${erroDetails.message}")
+            }
+            throw HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.message)
+        }
     }
 }
 
